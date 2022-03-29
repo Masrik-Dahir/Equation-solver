28475082f49c63fd01d73d6aa7ef6c27ec85005c
Masrik Dahir <dahirma@vcu.edu>
2022-03-29 19:40:54 -0400

    Initial commit

---
+++ src/main/java/com/masrik/demointerface/HelloController.java
@@ -0,0 +1,14 @@
+package com.masrik.demointerface;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.masrik.demointerface {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.masrik.demointerface to javafx.fxml;
+    exports com.masrik.demointerface;
+}
