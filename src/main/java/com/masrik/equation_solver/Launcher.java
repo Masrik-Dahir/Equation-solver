fe24cb3b3c24becae7710487dfbbd695fed7e06e
masri <dahirma@vcu.edu>
2022-04-03 00:52:31 -0400

    sad

---
+++ src/main/java/com/masrik/equation_solver/Launcher.java
@@ -0,0 +1,7 @@
+package com.masrik.equation_solver;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Future_value_euqation.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,6 @@ module com.masrik.demointerface {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
     opens com.masrik.equation_solver to javafx.fxml;
     exports com.masrik.equation_solver;
 }
